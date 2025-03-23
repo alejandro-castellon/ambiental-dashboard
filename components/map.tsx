14ae1f616407accdfe9e0fb757e9d61949af2c0a
Alejandro Castellon <alejandrocastellonfer@gmail.com>
2025-03-23 19:53:24 -0400

    finish overview chart

--- components/map.tsx
+++ components/map.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
 import L from "leaflet";
--- components/overview-chart.tsx
+++ components/overview-chart.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
 import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 import Link from "next/link";
 import {
@@ -20,79 +19,107 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 import { overviewChartData } from "@/lib/data";
-import { MapPoint } from "@/lib/types";
+import { MapPoint, ChartData } from "@/lib/types";
+import { useEffect, useState } from "react";
 
 const chartConfig = {
   ph: {
     label: "Ph",
     color: "hsl(var(--chart-1))",
   },
-  temp: {
+  temperature: {
     label: "T°",
     color: "red",
   },
-  con: {
+  conductivity: {
     label: "Con",
     color: "hsl(var(--chart-2))",
   },
 } satisfies ChartConfig;
 
+async function fetchSensorData(): Promise<ChartData[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(overviewChartData);
+    }, 1000);
+  });
+}
+
 export function OverviewChart({ point }: { point: MapPoint }) {
+  const [chartData, setChartData] = useState<ChartData[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchSensorData();
+      setChartData(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
     <Card className="my-4">
       <CardHeader>
         <CardTitle>Gráfico de {point.title}</CardTitle>
         <CardDescription>01 Abril - 07 Abril 2025</CardDescription>
       </CardHeader>
-      <CardContent>
-        <ChartContainer config={chartConfig}>
-          <LineChart
-            accessibilityLayer
-            data={overviewChartData}
-            margin={{
-              left: 12,
-              right: 12,
-            }}
-          >
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="date"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              tickFormatter={(value) => value.slice(0, 2)}
-            />
-            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
-            <Line
-              dataKey="ph"
-              type="monotone"
-              stroke="var(--color-ph)"
-              strokeWidth={2}
-              dot={false}
-            />
-            <Line
-              dataKey="temp"
-              type="monotone"
-              stroke="var(--color-temp)"
-              strokeWidth={2}
-              dot={false}
-            />
-            <Line
-              dataKey="con"
-              type="monotone"
-              stroke="var(--color-con)"
-              strokeWidth={2}
-              dot={false}
-            />
-            <ChartLegend content={<ChartLegendContent />} />
-          </LineChart>
-        </ChartContainer>
-      </CardContent>
+      {!loading && (
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="date"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 2)}
+              />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+              <Line
+                dataKey="ph"
+                type="monotone"
+                stroke="var(--color-ph)"
+                strokeWidth={2}
+                dot={false}
+              />
+              <Line
+                dataKey="temperature"
+                type="monotone"
+                stroke="var(--color-temperature)"
+                strokeWidth={2}
+                dot={false}
+              />
+              <Line
+                dataKey="conductivity"
+                type="monotone"
+                stroke="var(--color-conductivity)"
+                strokeWidth={2}
+                dot={false}
+              />
+              <ChartLegend content={<ChartLegendContent />} />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+      )}
+      {loading && (
+        <CardContent className="flex justify-center items-center">
+          <div className="animate-spin w-16 h-16 border-4 border-green-500 border-t-transparent rounded-full my-8"></div>
+        </CardContent>
+      )}
       <CardFooter>
         <div className="flex w-full items-start gap-2 text-sm">
           <div className="grid gap-2">
             <div className="flex items-center gap-2 font-medium leading-none">
-              Aumento de 5.2% esta semana <TrendingUp className="h-4 w-4" />
+              Datos obtenidos de la última semana.
             </div>
             <Link
               href={`/${point.id}`}
